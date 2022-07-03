0d15663fea9860055aaa4e4457e544a597ef2aeb
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-03 15:33:32 +1000

    Created AvailableMeals component and styles

---
+++ src/components/Meals/AvailableMeals.js
@@ -0,0 +1,46 @@
+import Card from '../UI/Card';
+import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem';
+
+function AvailableMeals() {
+  const meals = [
+    {
+      id: 'm1',
+      title: 'Sushi',
+      description: 'Finest fish and veggies',
+      price: 22.99,
+    },
+    {
+      id: 'm2',
+      title: 'Schnitzel',
+      description: 'A german specialtiy!',
+      price: 16.5,
+    },
+    {
+      id: 'm3',
+      title: 'Barbecue Burger',
+      description: 'American, raw, meaty',
+      price: 12.99,
+    },
+    {
+      id: 'm4',
+      title: 'Green Bowl',
+      description: 'Healty...and green...',
+      price: 18.99,
+    },
+  ];
+
+  return (
+    <section className={classes.meals}>
+      <Card>
+        <ul>
+          {meals.map(meal => (
+            <MealItem key={meal.id} item={meal} />
+          ))}
+        </ul>
+      </Card>
+    </section>
+  );
+}
+
+export default AvailableMeals;
