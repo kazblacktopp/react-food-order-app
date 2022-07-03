7f7fa2ff1492c4b8c0fe8a3892aa0a71090a811f
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-03 15:33:54 +1000

    Created MealItem component and styles

---
+++ src/components/Meals/MealItem.js
@@ -0,0 +1,19 @@
+import classes from './MealItem.module.css';
+import MealItemForm from './MealItemForm';
+
+function MealItem(props) {
+  return (
+    <li className={classes.meal}>
+      <div>
+        <h3>{props.item.title}</h3>
+        <div className={classes.description}>{props.item.description}</div>
+        <div className={classes.price}>${props.item.price}</div>
+      </div>
+      <div>
+        <MealItemForm id={props.item.id} />
+      </div>
+    </li>
+  );
+}
+
+export default MealItem;
