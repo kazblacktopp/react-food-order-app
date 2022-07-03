5cd9974246d31ec94b0779246c04c57d8a8acf6b
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-03 13:50:12 +1000

    Added MealsSummary component to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import { Fragment } from 'react';
 import Header from './components/Layout/Header';
+import MealsSummary from './components/Meals/MealsSummary';
 
 function App() {
   return (
     <Fragment>
       <Header />
-      <main></main>
+      <main>
+        <MealsSummary />
+      </main>
     </Fragment>
   );
 }
