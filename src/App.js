f41f14c1544394ab874fffff147f0a83b8133eb3
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-30 12:01:06 +1000

    Added Cart component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import CartProvider from './store/CartProvider';
 import Header from './components/Layout/Header';
 import MealsSummary from './components/Meals/MealsSummary';
 import AvailableMeals from './components/Meals/AvailableMeals';
-import Modal from './components/UI/Modal';
+import Modal from './components/UI/Modal/Modal';
+import Cart from './components/Cart/Cart';
 
 function App() {
   return (
     <CartProvider>
-      <Modal />
+      <Modal>
+        <Cart />
+      </Modal>
       <Header />
       <main>
         <MealsSummary />
