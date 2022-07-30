4a54b2386bac5188d857d9b88eabb8cd80deaab5
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-31 08:29:11 +1000

    Added add and remove item handlers using CartContext

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,19 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import Modal from '../UI/Modal/Modal';
 import CartItem from './CartItem';
 import classes from './Cart.module.css';
 
 export default function Cart(props) {
+  const [cartHasItems, setCartHasItems] = useState(false);
   const cartCtx = useContext(CartContext);
+  const totalAmount = cartCtx.totalAmount.toFixed(2);
   const cartItems = cartCtx.items.map(item => (
     <CartItem
       key={item.id}
       title={item.title}
       price={item.price}
       amount={item.amount}
+      onAddItem={addItemHandler.bind(null, item)}
+      onRemoveItem={removeItemHandler.bind(null, item.id)}
     />
   ));
+
+  if (cartHasItems && cartItems.length === 0) {
+    setCartHasItems(false);
+  }
+
+  if (!cartHasItems && cartItems.length !== 0) {
+    setCartHasItems(true);
+  }
+
+  function addItemHandler(item) {
+    cartCtx.addToCart({ ...item, amount: 1 });
+  }
+
+  function removeItemHandler(id) {
+    cartCtx.removeFromCart(id);
+  }
 
   return (
     <Modal>
@@ -21,7 +41,7 @@ export default function Cart(props) {
         <ul className={classes['cart-items']}>{cartItems}</ul>
         <div className={classes.total}>
           <span>Total Amount</span>
-          <span>$39.98</span>
+          <span>${totalAmount}</span>
         </div>
         <div className={classes.actions}>
           <button
@@ -30,7 +50,7 @@ export default function Cart(props) {
           >
             Close
           </button>
-          <button className={classes.button}>Order</button>
+          {cartHasItems && <button className={classes.button}>Order</button>}
         </div>
       </div>
     </Modal>
