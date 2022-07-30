ccf7b746ac4f0c32008c24e9f887c1b3737bab6d
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-30 17:47:11 +1000

    Add reducer function to control adding items to cart state

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -1,14 +1,55 @@
+import { useReducer } from 'react';
 import CartContext from './cart-context';
 
+const defaultCart = {
+  items: [],
+  totalAmount: 0,
+};
+
+function cartStateReducer(state, action) {
+  if (action.type === 'ADD-ITEM') {
+    const updatedTotalAmount =
+      state.totalAmount + action.item.amount * action.item.price;
+
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.item.id
+    );
+
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    let updatedCartItems;
+
+    if (existingCartItem) {
+      const updatedCartItem = {
+        ...action.item,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updatedCartItems = [...state.items];
+      updatedCartItems[existingCartItemIndex] = updatedCartItem;
+    } else {
+      updatedCartItems = [...state.items, action.item];
+    }
+    return { items: updatedCartItems, totalAmount: updatedTotalAmount };
+  }
+  return state;
+}
+
 export default function CartProvider(props) {
+  const [cartState, dispatchCartReducer] = useReducer(
+    cartStateReducer,
+    defaultCart
+  );
+
   const cartContext = {
-    items: [],
-    totalAmount: 0,
+    items: cartState.items,
+    totalAmount: cartState.totalAmount,
     addToCart: addToCartHandler,
     removeFromCart: removeFromCartHandler,
   };
 
-  function addToCartHandler(newItem) {}
+  function addToCartHandler(newItem) {
+    dispatchCartReducer({ type: 'ADD-ITEM', item: newItem });
+  }
 
   function removeFromCartHandler(id) {}
 
