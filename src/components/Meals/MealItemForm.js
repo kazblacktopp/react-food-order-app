990b4e2f7a73279c791b84bf6c4e64bfeb163d10
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-30 15:27:55 +1000

    Add inputRef and lift inputRef value up to parent component

--- src/components/Meals/MealItemForm.js
+++ src/components/Meals/MealItemForm.js
@@ -1,14 +1,20 @@
+import { useRef } from 'react';
 import Input from '../UI/Input';
 import classes from './MealItemForm.module.css';
 
 export default function MealItemForm(props) {
+  const inputRef = useRef();
+
   function formSubmitHandler(event) {
     event.preventDefault();
+    const amount = inputRef.current.value;
+    props.onAddItem(amount);
   }
 
   return (
     <form className={classes.form} onSubmit={formSubmitHandler}>
       <Input
+        ref={inputRef}
         id={`amount_${props.id}`}
         label="Amount"
         type="number"
