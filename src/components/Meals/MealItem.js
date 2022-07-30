c422520f48e52d06d8caa19cab9f05caa2795c97
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-30 20:02:29 +1000

    Fix item price to 2 decimal places

--- src/components/Meals/MealItem.js
+++ src/components/Meals/MealItem.js
@@ -5,6 +5,7 @@ import classes from './MealItem.module.css';
 
 export default function MealItem(props) {
   const cartCtx = useContext(CartContext);
+  const price = props.item.price.toFixed(2);
 
   function addItemToCartHandler(amount) {
     cartCtx.addToCart({ ...props.item, amount: amount });
@@ -15,7 +16,7 @@ export default function MealItem(props) {
       <div>
         <h3>{props.item.title}</h3>
         <div className={classes.description}>{props.item.description}</div>
-        <div className={classes.price}>${props.item.price}</div>
+        <div className={classes.price}>${price}</div>
       </div>
       <div>
         <MealItemForm id={props.item.id} onAddItem={addItemToCartHandler} />
