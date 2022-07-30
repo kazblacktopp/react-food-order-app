5b903538309134a9ef535319e1c63fc72c67d9b7
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-30 13:09:20 +1000

    Created cartItems constant to extract array items

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,20 @@ import classes from './Cart.module.css';
 import CartItem from './CartItem';
 
 export default function Cart(props) {
+  const cartItems = [
+    { id: 'item-1', name: 'Sushi', price: 22.98, amount: 1 },
+  ].map(item => (
+    <CartItem
+      key={item.id}
+      name={item.name}
+      price={item.price}
+      amount={item.amount}
+    />
+  ));
+
   return (
     <div>
-      <ul className={classes['cart-items']}>
-        <CartItem key="item-1" name="Sushi" price="22.98" amount="1" />
-      </ul>
+      <ul className={classes['cart-items']}>{cartItems}</ul>
       <div className={classes.total}>
         <span>Total Amount</span>
         <span>$39.98</span>
