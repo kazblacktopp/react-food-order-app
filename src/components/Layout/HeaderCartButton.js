325b8e5bbf95b4fcb6eafc5da1f44684261dd2e0
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-31 18:38:21 +1000

    Use useEffect to apply and remove class to header cart button

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,39 @@
-import { useContext } from 'react';
+import { useContext, useEffect, useRef, useState } from 'react';
 import CartContext from '../../store/cart-context';
 import CartIcon from '../Cart/CartIcon';
 import classes from './HeaderCartButton.module.css';
 
 export default function HeaderCartButton(props) {
+  const [cartDidUpdate, setCartDidUpdate] = useState(false);
+  const isMounted = useRef(false);
   const cartCtx = useContext(CartContext);
+
   const cartQuantity = cartCtx.items.reduce((currQuantity, item) => {
     return currQuantity + item.amount;
   }, 0);
 
+  const btnClasses = `${classes.button} ${cartDidUpdate ? classes.bump : ''}`;
+
+  const { items } = cartCtx;
+
+  useEffect(() => {
+    if (isMounted.current) {
+      setCartDidUpdate(true);
+
+      const timer = setTimeout(() => {
+        setCartDidUpdate(false);
+      }, 300);
+
+      return () => {
+        clearTimeout(timer);
+      };
+    } else {
+      isMounted.current = true;
+    }
+  }, [items]);
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
