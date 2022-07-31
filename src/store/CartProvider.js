6ac2bebae51edb66057e34d7625cbab10db3d52d
kazblacktopp <karen.blacktopp@gmail.com>
2022-07-31 17:52:39 +1000

    Added logic for removing item from CartContext

--- src/store/CartProvider.js
+++ src/store/CartProvider.js
@@ -7,6 +7,8 @@ const defaultCart = {
 };
 
 function cartStateReducer(state, action) {
+  let updatedCartItems;
+
   if (action.type === 'ADD-ITEM') {
     const updatedTotalAmount =
       state.totalAmount + action.item.amount * action.item.price;
@@ -16,8 +18,6 @@ function cartStateReducer(state, action) {
     );
 
     const existingCartItem = state.items[existingCartItemIndex];
-
-    let updatedCartItems;
 
     if (existingCartItem) {
       const updatedCartItem = {
@@ -31,6 +31,32 @@ function cartStateReducer(state, action) {
     }
     return { items: updatedCartItems, totalAmount: updatedTotalAmount };
   }
+
+  if (action.type === 'REMOVE-ITEM') {
+    const existingCartItemIndex = state.items.findIndex(
+      item => item.id === action.id
+    );
+    const existingCartItem = state.items[existingCartItemIndex];
+
+    const updatedTotalAmount = Math.abs(
+      state.totalAmount - existingCartItem.price
+    );
+
+    const updatedCartItem = {
+      ...existingCartItem,
+      amount: existingCartItem.amount - 1,
+    };
+
+    if (updatedCartItem.amount === 0) {
+      updatedCartItems = state.items.filter(item => item.id !== action.id);
+    } else {
+      updatedCartItems = [...state.items];
+      updatedCartItems[existingCartItemIndex] = updatedCartItem;
+    }
+
+    return { items: updatedCartItems, totalAmount: updatedTotalAmount };
+  }
+
   return state;
 }
 
@@ -51,7 +77,9 @@ export default function CartProvider(props) {
     dispatchCartReducer({ type: 'ADD-ITEM', item: newItem });
   }
 
-  function removeFromCartHandler(id) {}
+  function removeFromCartHandler(id) {
+    dispatchCartReducer({ type: 'REMOVE-ITEM', id: id });
+  }
 
   return (
     <CartContext.Provider value={cartContext}>
